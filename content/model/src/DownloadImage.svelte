ed7576f828455aaff43c106cf5f779afb763b34c
Galloway <emg33@ad.unc.edu>
2020-12-10 14:04:14 -0500

    tweaked download line chart image to fit all parameter descriptions and increase height based on number of legend items

--- content/model/src/DownloadImage.svelte
+++ content/model/src/DownloadImage.svelte
@@ -20,39 +20,56 @@
       legendArray.push({ color, legendText });
     }
 
-    const maxFieldLength = 23;
     const maxFieldLengthArray = legendArray.reduce(function(acc, curr) {
       curr.legendText.forEach(function(d, i) {
-        if (d.length > acc[i] || acc[i] === undefined)
-          acc[i] = d.length > maxFieldLength ? maxFieldLength : d.length;
+        if (d.length > acc[i] || acc[i] === undefined) acc[i] = d.length;
       });
       return acc;
     }, []);
 
     const legendLine = legendArray.map(function(d) {
-      const legendText = d.legendText
-        .map(function(e, i) {
-          let currString = e;
-          if (e.length > maxFieldLength) {
-            currString = e.slice(0, maxFieldLength - 3) + "...";
-          } else {
-            currString = e.padEnd(maxFieldLengthArray[i]);
-          }
-          return currString;
-        })
-        .join("   ");
-      return { color: d.color, legendText };
+      const legendText = d.legendText.map(function(e, i) {
+        return e.padEnd(maxFieldLengthArray[i]);
+      });
+
+      //Cut up into lines to allow line wrapping
+      const maxLineLength = 100;
+      let legendSubLines = [];
+      let legendSubLinesIndex = 0;
+      let currentLineLength = 0;
+      for (let i = 0; i < legendText.length; i++) {
+        if (currentLineLength < maxLineLength) {
+          legendSubLines[legendSubLinesIndex] =
+            legendSubLines[legendSubLinesIndex] == undefined
+              ? legendText[i] + "   "
+              : legendSubLines[legendSubLinesIndex] + legendText[i] + "   ";
+          currentLineLength = legendSubLines[legendSubLinesIndex].length;
+        } else {
+          legendSubLinesIndex++;
+          legendSubLines[legendSubLinesIndex] = legendText[i] + "   ";
+          currentLineLength = 0;
+        }
+      }
+      legendSubLines = legendSubLines.map(e => e.trim());
+      return { color: d.color, legendSubLines };
     });
 
     //Get SVG, format, and add elements
-    const width = 1050;
-    const height = 810;
+    const width = 900;
+
+    const lineHeight = 15;
+    const numberOfLines = legendLine
+      .map(d => [...d.legendSubLines, "blank line"])
+      .reduce((acc, val) => acc.concat(val), []).length;
+    const heightOfLines = numberOfLines * lineHeight + 50; //50 is some padding
+    const height = 580 + heightOfLines;
+
     const svg = document.getElementById("line-chart-svg").cloneNode(true);
     svg.setAttribute("viewBox", `0 0 ${width} ${height}`);
     svg.style.fontFamily = "Helvetica, Arial, sans-serif";
     svg
       .querySelector(".chart-container")
-      .setAttribute("transform", "translate(0, 80)");
+      .setAttribute("transform", "translate(20, 100)");
 
     //Title
     const titleText = document.createElementNS(
@@ -60,7 +77,7 @@
       "text"
     );
     titleText.setAttributeNS(null, "font-size", "30px");
-    titleText.setAttributeNS(null, "transform", `translate(20,30)`);
+    titleText.setAttributeNS(null, "transform", `translate(40,50)`);
     titleText.innerHTML = title;
 
     //Subtitle
@@ -69,7 +86,7 @@
       "text"
     );
     subtitleText.setAttributeNS(null, "font-size", "20px");
-    subtitleText.setAttributeNS(null, "transform", `translate(20,60)`);
+    subtitleText.setAttributeNS(null, "transform", `translate(40,80)`);
     subtitleText.innerHTML = subtitle;
 
     //Legend
@@ -78,7 +95,9 @@
       group.setAttributeNS(
         null,
         "transform",
-        `translate(30, ${height - 250 + i * 25})`
+        `translate(40, ${height -
+          heightOfLines +
+          i * (lineHeight + d.legendSubLines.length * lineHeight)})`
       );
 
       const text = document.createElementNS(
@@ -87,11 +106,22 @@
       );
       text.style.fontFamily = "monospace";
       text.style.whiteSpace = "pre";
-      text.setAttributeNS(null, "font-size", 14);
-      text.setAttributeNS(null, "dx", 50);
-      text.setAttributeNS(null, "dy", 15);
-      const textNode = document.createTextNode(d.legendText);
-      text.appendChild(textNode);
+      text.setAttributeNS(null, "font-size", 12);
+      const x = 50;
+      text.setAttributeNS(null, "x", x);
+      text.setAttributeNS(null, "dy", lineHeight);
+
+      d.legendSubLines.forEach(function(l) {
+        let svgTSpan = document.createElementNS(
+          "http://www.w3.org/2000/svg",
+          "tspan"
+        );
+        svgTSpan.setAttributeNS(null, "x", x);
+        svgTSpan.setAttributeNS(null, "dy", 1.3 + "em");
+        let tSpanTextNode = document.createTextNode(l);
+        svgTSpan.appendChild(tSpanTextNode);
+        text.appendChild(svgTSpan);
+      });
 
       const rect = document.createElementNS(
         "http://www.w3.org/2000/svg",
@@ -114,7 +144,8 @@
 
     // console.log(svg);
     saveSvgAsPng.saveSvgAsPng(svg, "nurse_line_chart.png", {
-      backgroundColor: "#fff"
+      backgroundColor: "#fff",
+      scale: 2
     });
   }
 
